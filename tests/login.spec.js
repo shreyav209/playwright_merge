7b513381565252ec9e16cd4cf2ef35529ea45c59
shreya.vethekar09@gmail.com <shreyav209>
2024-08-30 21:14:06 +0530

    Initial commit

---
+++ tests/login.spec.js
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://practicetestautomation.com/practice-test-login/');
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill('student');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('Password123');
+}); 
